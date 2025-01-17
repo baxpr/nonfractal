ed6b0d652a71be98ed96e8da8f498a3f6a164ea5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-01-17 11:37:38 -0600

    typo-wmtsa

--- wmtsa-matlab-0.2.6/wmtsa/odwt/@omodwt/omodwt.m
+++ wmtsa-matlab-0.2.6/wmtsa/odwt/@omodwt/omodwt.m
@@ -1,54 +1,55 @@
 classdef omodwt
-% file:  @omodwt/omodwt.m
-% Public Properties
-  properties (SetAccess = 'private')
-    X                  = [];
-    WJ                 = [];
-    VJ                 = [];
-                       
-    WTF                = '';
-    NX                 = 0;
-    NW                 = 0;
-    J0                 = 0;
-    NChan              = 0;
-    Boundary           = '';
-    Aligned            = false;
-    RetainedVJ         = false;
-    
- 
-% Class methods
-  methods
-  
-  
-    function obj = omodwt(X, wtfname, nlevels, boundary, varargin)
-    % Constructor
-      obj.X = X;
-      defaults.wtfname  = 'la8';
-      defaults.boundary = 'reflection';
-      defaults.nlevels  = 'conservative';
-      set_defaults(defaults);
+    % file:  @omodwt/omodwt.m
+    % Public Properties
+    properties (SetAccess = 'private')
+        X                  = [];
+        WJ                 = [];
+        VJ                 = [];
 
-      if (nargin > 4)
-        [WJ, VJ, w_att] = modwt(X, wtfname, nlevels, boundary, varargin);
-      else
-        [WJ, VJ, w_att] = modwt(X, wtfname, nlevels, boundary);
-      end
-      obj.WJ = WJ;
-      obj.VJ = VJ;
+        WTF                = '';
+        NX                 = 0;
+        NW                 = 0;
+        J0                 = 0;
+        NChan              = 0;
+        Boundary           = '';
+        Aligned            = false;
+        RetainedVJ         = false;
+    end
 
-      obj = set_properties_from_att(obj, w_att);
+    % Class methods
+    methods
 
-    end % Constructor
-        
-    function plot(obj)
-      w_att = att(obj);
-      plot_modwt_coef(obj.WJ, obj.VJ, obj.X, w_att); 
-    end % plot
-        
-    function att = att(obj)
-      att = get_att_from_properties(obj);
-    end % att
-        
+        function obj = omodwt(X, wtfname, nlevels, boundary, varargin)
+            % Constructor
+            obj.X = X;
+            defaults.wtfname  = 'la8';
+            defaults.boundary = 'reflection';
+            defaults.nlevels  = 'conservative';
+            set_defaults(defaults);
+
+            if (nargin > 4)
+                [WJ, VJ, w_att] = modwt(X, wtfname, nlevels, boundary, varargin);
+            else
+                [WJ, VJ, w_att] = modwt(X, wtfname, nlevels, boundary);
+            end
+            obj.WJ = WJ;
+            obj.VJ = VJ;
+
+            obj = set_properties_from_att(obj, w_att);
+
+        end % Constructor
+
+        function plot(obj)
+            w_att = att(obj);
+            plot_modwt_coef(obj.WJ, obj.VJ, obj.X, w_att);
+        end % plot
+
+        function att = att(obj)
+            att = get_att_from_properties(obj);
+        end % att
+
+    end % Methods
+
 end % Class
-        
-        
+
+
